248d1d8a8fb4217a67c9901af2618bf2aa983eda
Jianhui Ben <jianhui.ben@gmail.com>
2020-10-16 00:34:28 -0700

    10/15/2020

--- by tags/arrays/152. Maximum Product Subarray.py
+++ by tags/arrays/152. Maximum Product Subarray.py
@@ -14,5 +14,19 @@
 #Output: 0
 #Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
 
+##dynamic programming
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+
+        if len(nums)==1:
+            return nums[0]
+        dp, small, large=[_ for _ in range(len(nums))], [_ for _ in range(len(nums))], [_ for _ in range(len(nums))]
+        dp[0], small[0], large[0]= nums[0],nums[0],nums[0]
+        for i in range(1, len(nums)):
+            large[i]= max([large[i-1]*nums[i], small[i-1]*nums[i], nums[i]])
+            small[i]= min([large[i-1]*nums[i], small[i-1]*nums[i], nums[i]])
+            dp[i]= large[i]
+            
+        return max(dp)
 
 
