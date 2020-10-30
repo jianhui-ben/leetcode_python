0dc522ff01574fdb92c31f85c171a3af94303fe1
Jianhui Ben <jianhui.ben@gmail.com>
2020-10-30 11:58:12 -0700

    10/30/2020

--- by tags/dynamic programming/377. Combination Sum IV.py
+++ by tags/dynamic programming/377. Combination Sum IV.py
@@ -25,3 +25,26 @@
 #What if negative numbers are allowed in the given array?
 #How does it change the problem?
 #What limitation we need to add to the question to allow negative numbers?
+
+
+##dp: time O(target* len(nums)), space: O(target)
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        dp=[0 for _ in range(target)]
+        nums.sort()
+        cur_index=0
+        for i in range(target):
+            x= 0
+            if i+1 in nums:
+                x+=1
+                cur_index= nums.index(i+1)
+            for k in nums[: cur_index+1]:
+                if i>=k:
+                    x+=dp[i-k]
+            dp[i]=x
+        return dp[-1]
+
+##follow-up
+##if a negative number is allowed, then there'd be infinite number of combinations
+## so we have to add a limitation to the problem saying each number in the nums list is only
+## allowed to be used once.
