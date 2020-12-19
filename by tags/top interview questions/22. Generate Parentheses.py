da264e2053cd495089eb63fc6bdc3bb5a4b2bd83
Jianhui Ben <jianhui.ben@gmail.com>
2020-12-18 20:19:15 -0800

    12/18/2020

---
+++ by tags/top interview questions/22. Generate Parentheses.py
@@ -0,0 +1,29 @@
+#22. Generate Parentheses
+
+#Given n pairs of parentheses, write a function to generate all combinations
+#of well-formed parentheses.
+
+#Example 1:
+
+#Input: n = 3
+#Output: ["((()))","(()())","(())()","()(())","()()()"]
+#Example 2:
+
+#Input: n = 1
+#Output: ["()"]
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        self.out=[]
+        def recursion(left, right, cur_str):
+            if left>=0 and right>=0:
+                if left==0:
+                    cur_str+= right*')'
+                    self.out.append(cur_str)
+                    return
+                if left<right:
+                    recursion(left, right-1, cur_str+')')
+                recursion(left-1, right, cur_str+'(')
+                
+        recursion(n, n, '')
+        return self.out
---
+++ by tags/top interview questions/46. Permutations.py
@@ -0,0 +1,30 @@
+#46. Permutations
+
+#Given an array nums of distinct integers, return all the possible permutations. 
+#You can return the answer in any order.
+
+ 
+
+#Example 1:
+
+#Input: nums = [1,2,3]
+#Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+#Example 2:
+
+#Input: nums = [0,1]
+#Output: [[0,1],[1,0]]
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.out=[]
+        def recusion(cur_arr, left):
+            if not left:
+                self.out.append(cur_arr)
+                return
+            for i in left:
+                recusion(cur_arr+[i], [k for k in left if k!=i])
+                ##avoid duplicates
+                # recusion([i]+cur_arr, [k for k in left if k!=i])
+        recusion([], nums)
+
+        return self.out
